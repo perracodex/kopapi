210b7e3d192c287265f8e1e7dcb7200a43d39ae4
Perraco <perracolabs@gmail.com>
2024-10-03 00:11:43 +0200

    Cleanup

--- src/main/kotlin/io/github/perracodex/kopapi/routing/ExtractEndpointPath.kt
+++ src/main/kotlin/io/github/perracodex/kopapi/routing/ExtractEndpointPath.kt
@@ -2,35 +2,9 @@
  * Copyright (c) 2024-Present Perracodex. Use of this source code is governed by an MIT license.
  */
 
-package io.github.perracodex.kopapi.parser
+package io.github.perracodex.kopapi.routing
 
-import io.ktor.server.application.*
 import io.ktor.server.routing.*
-import io.ktor.util.*
-
-/**
- * Traverse all routes and collect a given attribute from each route.
- *
- * @param attributeKey The attribute key to collect from each route.
- * @return A list of all found attribute values.
- */
-internal fun <T : Any> Application.collectRouteAttributes(attributeKey: AttributeKey<T>): List<T> {
-    val attributeValues: MutableList<T> = mutableListOf()
-
-    // Helper function to recursively traverse routes and collect attribute values.
-    fun Route.collectAttributes() {
-        this.attributes.getOrNull(key = attributeKey)?.let {
-            attributeValues.add(it)
-        }
-        // Recursively collect attributes from child routes.
-        this.children.forEach { it.collectAttributes() }
-    }
-
-    // Start collecting from the root route.
-    this.routing { }.collectAttributes()
-
-    return attributeValues
-}
 
 /**
  * Constructs the full path of a route by aggregating path segments from the current route up to the root,
