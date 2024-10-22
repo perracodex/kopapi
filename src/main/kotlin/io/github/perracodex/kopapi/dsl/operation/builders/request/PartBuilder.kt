71606a42e6b667694bd8470c723d35c23b186298
Perraco <perracolabs@gmail.com>
2024-10-22 20:30:14 +0200

    Removed unnecessary deprecated field from PartBuilder

--- src/main/kotlin/io/github/perracodex/kopapi/dsl/operation/builders/request/PartBuilder.kt
+++ src/main/kotlin/io/github/perracodex/kopapi/dsl/operation/builders/request/PartBuilder.kt
@@ -13,13 +13,11 @@ import io.github.perracodex.kopapi.utils.string.MultilineString
  * @property name The name of the part.
  * @property required Indicates whether the part is mandatory for the multipart request. Defaults to `true`.
  * @property description Optional description of the part.
- * @property deprecated Indicates whether the part is deprecated. Defaults to `false`.
  */
 @OperationDsl
 public class PartBuilder @PublishedApi internal constructor(
     public val name: String,
-    public var required: Boolean = true,
-    public var deprecated: Boolean = false
+    public var required: Boolean = true
 ) {
     public var description: String by MultilineString()
 }
