3f2b3adcb717fa5393e0d44f265aeb6f7e15585b
Perraco <perracolabs@gmail.com>
2024-11-01 16:26:18 +0100

    Updated docs

--- src/main/kotlin/io/github/perracodex/kopapi/inspector/schema/factory/SchemaFactory.kt
+++ src/main/kotlin/io/github/perracodex/kopapi/inspector/schema/factory/SchemaFactory.kt
@@ -17,64 +17,78 @@ import io.github.perracodex.kopapi.types.ApiType
  */
 @TypeInspectorAPI
 internal object SchemaFactory {
+    /** Creates a specification entry for an `object` type. */
     fun ofObject(): ElementSchema.Object {
         return ElementSchema.Object(properties = mutableMapOf())
     }
 
+    /** Creates a specification entry for a `string` primitive type. */
     fun ofString(): ElementSchema.Primitive {
         return ElementSchema.Primitive(schemaType = ApiType.STRING)
     }
 
+    /** Creates a specification entry for a `char` primitive type. */
     fun ofChar(): ElementSchema.Primitive {
         return ElementSchema.Primitive(schemaType = ApiType.STRING, minLength = 1, maxLength = 1)
     }
 
+    /** Creates a specification entry for an `integer` primitive type. */
     fun ofInt32(): ElementSchema.Primitive {
         return ElementSchema.Primitive(schemaType = ApiType.INTEGER, format = ApiFormat.INT32())
     }
 
+    /** Creates a specification entry for a `long` primitive type. */
     fun ofInt64(): ElementSchema.Primitive {
         return ElementSchema.Primitive(schemaType = ApiType.INTEGER, format = ApiFormat.INT64())
     }
 
+    /** Creates a specification entry for a `float` primitive type. */
     fun ofFloat(): ElementSchema.Primitive {
         return ElementSchema.Primitive(schemaType = ApiType.NUMBER, format = ApiFormat.FLOAT())
     }
 
+    /** Creates a specification entry for a `double` primitive type. */
     fun ofDouble(): ElementSchema.Primitive {
         return ElementSchema.Primitive(schemaType = ApiType.NUMBER, format = ApiFormat.DOUBLE())
     }
 
+    /** Creates a specification entry for a `uuid` primitive type. */
     fun ofUuid(): ElementSchema.Primitive {
         return ElementSchema.Primitive(schemaType = ApiType.STRING, format = ApiFormat.UUID())
     }
 
+    /** Creates a specification entry for a `date` primitive type. */
     fun ofDate(): ElementSchema.Primitive {
         return ElementSchema.Primitive(schemaType = ApiType.STRING, format = ApiFormat.DATE())
     }
 
+    /** Creates a specification entry for a `time` primitive type. */
     fun ofDateTime(): ElementSchema.Primitive {
         return ElementSchema.Primitive(schemaType = ApiType.STRING, format = ApiFormat.DATETIME())
     }
 
+    /** Creates a specification entry for a `time` primitive type. */
     fun ofTime(): ElementSchema.Primitive {
         return ElementSchema.Primitive(schemaType = ApiType.STRING, format = ApiFormat.TIME())
     }
 
+    /** Creates a specification entry for a `uri` primitive type. */
     fun ofUri(): ElementSchema.Primitive {
         return ElementSchema.Primitive(schemaType = ApiType.STRING, format = ApiFormat.URI())
     }
 
+    /** Creates a specification entry for a `byte` primitive type. */
     fun ofByte(): ElementSchema.Primitive {
         return ElementSchema.Primitive(schemaType = ApiType.STRING, format = ApiFormat.BYTE())
     }
 
+    /** Creates a specification entry for a `boolean` primitive type. */
     fun ofBoolean(): ElementSchema.Primitive {
         return ElementSchema.Primitive(schemaType = ApiType.BOOLEAN)
     }
 
     /**
-     * Creates a specification entry for an enumeration of values.
+     * Creates a specification entry for an `enumeration` of values.
      *
      * @param values The list of values to enumerate.
      * @return The [ElementSchema.Enum] for the enumeration.
@@ -84,7 +98,7 @@ internal object SchemaFactory {
     }
 
     /**
-     * Creates a specification entry for an array of items.
+     * Creates a specification entry for an `array` of items.
      *
      * @param items The schema items in the collection.
      * @return  The [ElementSchema.Array] for the array.
@@ -94,7 +108,7 @@ internal object SchemaFactory {
     }
 
     /**
-     * Creates a specification entry for a collection of items (e.g., `List` or `Set`).
+     * Creates a specification entry for a `collection` of items (e.g., `List` or `Set`).
      *
      * @param items The schema items in the collection.
      * @return The [ElementSchema.Array] for the collection.
@@ -114,7 +128,7 @@ internal object SchemaFactory {
     }
 
     /**
-     * Creates a specification entry for a reference to a schema.
+     * Creates a specification entry for a `reference` to a schema.
      *
      * @param schemaName The name of the schema to reference.
      * @return The [ElementSchema.Reference]] instance for the schema reference.
